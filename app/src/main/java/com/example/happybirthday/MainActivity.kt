329bc4331ecf2bafe99c229c91869ef32d65253a
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-04-25 20:39:07 +0900

    완료

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.happybirthday
 
 import android.os.Bundle
+import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +13,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +26,35 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    
+                    BirthdayGreetingWithText(message = "Happy Birthday 한정우!", from = "-From 한정우")
                 }
             }
         }
     }
 }
-
-@Preview(showBackground = true)
-@Composable
-fun BirthdayCardPreview() {
-    HappyBirthdayTheme {
-        Greeting("한정우")
-    }
-}
-
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
+@Composable
+fun BirthdayGreetingWithText(message: String , from: String , modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = message,
+            fontSize = 36.sp
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp
+        )
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        BirthdayGreetingWithText(message = "Happy Birthday 한정우!", from = "-From 한정우")
+    }
+}
+
+
